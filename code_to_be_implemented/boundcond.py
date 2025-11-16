3afd3b23d82fc549ed11e3f1e7bf7c66c88e60fc
Maximilien Zarioh <maximilien003@gmail.com>
2025-11-16 13:06:36 +0100

    session 2

--- code_to_be_implemented/boundcond.py
+++ code_to_be_implemented/boundcond.py
@@ -35,24 +35,58 @@ def boundcond(pdof: np.ndarray,
     `return Kcl, Fcl`, those values are automatically bundled into a tuple.
     """
 
-    # TODO: Total number of degrees of freedom of the system
-    ndof = 
+    # get the total number of degrees of freedom of the system
+    ndof = 3 * nnode
 
     # build dofpos
     dofpos = compute_dofpos(nnode)
 
-    # TODO: Number of nodal degrees of freedom
-    ndof_node = 
+    # get the number of nodal degrees of freedom
+    ndof_node = 3
 
-    # TODO: Initialize Kcl and Fcl 
-    # !!! Numpy arrays are mutable, so can be modified outside of the function scope. See .copy()
-    Fcl = 
-    Kcl = 
+    # initialize Kcl and Fcl (Numpy arrays are mutable, so can be modified outside the function scope)
+    Fcl = Fext.copy()
+    Kcl = Ksys.copy()
 
-    if method < 0:
-        # TODO: Direct method
-    else:
-        # TODO: Penalty method
-        Z = float(method)
+    if method < 0: # Direct method
+        # loop over each prescribed degree of freedom
+        for i in range(pdof.shape[0]):
+            # get node number, direction and value
+            node_num = int(pdof[i,0]) - 1 # 0-based index
+            node_dir = int(pdof[i,1]) - 1 # 0-based index
+            node_val = pdof[i,2]
+
+            # get the global dof position in the matrix
+            node_pos = dofpos[node_num,node_dir]
+
+            # step 1 (cf slides): group known terms on RHS
+            Fcl = Fcl - Ksys[:,node_pos] * node_val
+
+            # step 2: "remove" equation by modifying Kcl and Fcl
+            # edit Kcl (put 0s to the row and column)
+            Kcl[node_pos,:] = 0
+            Kcl[:,node_pos] = 0
+            # put 1 to the diagonal element of Kcl
+            Kcl[node_pos,node_pos] = 1
+            # put the value tu the element of Fcl
+            Fcl[node_pos] = node_val
+
+    else: # Penalty method
+        Z = method
+        # loop over each prescribed degree of freedom
+        for i in range(pdof.shape[0]):
+            # get node number, direction, and value
+            node_num = int(pdof[i,0]) - 1 # 0-based index
+            node_dir = int(pdof[i,1]) - 1 # 0-based index
+            node_val = pdof[i, 2]
+
+            # get the global dof position in the matrix
+            node_pos = dofpos[node_num, node_dir]
+
+            # add the penalty term to the diagonal of the stiffness matrix
+            Kcl[node_pos, node_pos] += Z
+
+            # add the penalty term to the force vector
+            Fcl[node_pos] = Fcl[node_pos] + Z*node_val
 
     return Kcl, Fcl
